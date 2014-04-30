ace2f648c55879b648f76926a9c5a39248059403
maxatma <maxatma@gmail.com>
2014-04-30 13:11:37 +0300

    Initial Commit

---
+++ Array/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Array
+//
+//  Created by Запорожченко Александр Михайлович on 30.04.14.
+//  Copyright (c) 2014 IDS Outsource. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
